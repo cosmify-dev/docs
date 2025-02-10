5c7181fee95918eeab3105a1907f6a2cd37ebbf8
undefinedhuman <alexanderpadberg33@gmail.com>
2025-02-10 17:58:45 +0100

    feat: add page seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,23 @@ export default defineNuxtConfig({
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     },
   },
+  seo: {
+    meta: {
+      description: "Own Your Platform. Self-Host Your PaaS with Cosmify.",
+      themeColor: [
+        { content: "#18181b", media: "(prefers-color-scheme: dark)" },
+        { content: "white", media: "(prefers-color-scheme: light)" },
+      ],
+      author: "Alexander Padberg <undefinedhuman>",
+      colorScheme: "dark light",
+      applicationName: "Cosmify",
+      ogSiteName: "Cosmify",
+      ogLocale: "en_US",
+      ogType: "website",
+      ogUrl: "https://cosmify.dev",
+      ogTitle: "Cosmify",
+    },
+  },
   site: {
     url: "https://cosmify.dev",
     name: "Cosmify Documentation",
